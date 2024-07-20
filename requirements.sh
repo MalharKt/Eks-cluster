a8264805da2ef14c20f5566cf35f5aa23e8cfa63
Malhar <malharkauthale2019@gmail.com>
2024-07-20 12:41:22 +0530

    requirements added

---
+++ requirements.sh
@@ -0,0 +1,31 @@
+#Update the packages
+
+sudo apt update
+
+#Install aws-cli  
+
+sudo snap install aws-cli --classic
+
+#Install Docker
+
+sudo apt install docker.io -y
+docker version
+
+#Install eks-ctl
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version
+
+#Install kube-ctl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
